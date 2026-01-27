86a59cb3c1189121f7f65004064969a2bc215072
Teodora Cholakova <tedycholakova@gmail.com>
2026-01-27 14:45:09 +0200

    Initial project setup with Vite, Tailwind, React and TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="p-10 bg-blue-500 text-white text-3xl rounded-xl">
+        Tailwind работи! 🎉
+      </div>
+    </>
+  );
+}
+
+export default App;
